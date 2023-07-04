b6bd4be47f35f403cb8a4d1865a654435d62f9b9
abaferas <wasfyelgamed@gmail.com>
2023-07-04 04:56:40 +0300

    implement type, difficulties and category enums

---
+++ data/src/main/java/com/redvelvet/data/utils/QuizCategory.kt
@@ -0,0 +1,13 @@
+package com.redvelvet.data.utils
+
+enum class QuizCategory(val categoryName: String) {
+    SCIENCE("Science"),
+    FILM_AND_TV("Film and TV"),
+    MUSIC("Music"),
+    HISTORY("History"),
+    GEOGRAPHY("Geography"),
+    ART_AND_LITERATURE("Art and Literature"),
+    SPORT_AND_LEISURE("Sport and Leisure"),
+    GENERAL_KNOWLEDGE("General Knowledge"),
+    FOOD_AND_DRINK("Food and Drink")
+}
---
+++ data/src/main/java/com/redvelvet/data/utils/QuizType.kt
@@ -0,0 +1,6 @@
+package com.redvelvet.data.utils
+
+enum class QuizType(val type: String) {
+    IMAGE("image_choice"),
+    TEXT("text_choice")
+}
