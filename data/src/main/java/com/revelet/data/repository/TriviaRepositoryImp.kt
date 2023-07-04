41cf8aa85fdc666c23fa9fa18ed536a90810c76e
Haidy <haidyabugom3a@gmail.com>
2023-07-05 01:24:13 +0300

    add question and answer mappers

--- data/src/main/java/com/revelet/data/repository/TriviaRepositoryImp.kt
+++ data/src/main/java/com/revelet/data/repository/TriviaRepositoryImp.kt
@@ -3,31 +3,13 @@ package com.revelet.data.repository
 import com.redvelvet.domain.repository.ITriviaRepository
 
 class TriviaRepositoryImp() : ITriviaRepository {
-    override suspend fun getQuestionsRandomly() {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getQuestionsByLimit(limit: Int, types: String) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getQuestionsByTag(limit: Int, vararg tag: String, types: String) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun getQuestionsByLevel(
-        limit: Int,
-        vararg difficulties: String,
-        types: String
+    override suspend fun getRandomSetOfQuestion(
+        limit: Int?,
+        categories: String?,
+        difficulties: String?,
+        types: String?
     ) {
         TODO("Not yet implemented")
     }
 
-    override suspend fun getQuestionsByCategories(
-        limit: Int,
-        vararg category: String,
-        types: String
-    ) {
-        TODO("Not yet implemented")
-    }
 }
