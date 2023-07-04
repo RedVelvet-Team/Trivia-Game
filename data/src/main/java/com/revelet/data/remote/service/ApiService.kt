ff619b8626b9e14805d3a0b21fe5cb49e9f4c3cf
Bassam <bassamouf@std.mans.edu.eg>
2023-07-04 19:54:11 +0300

    edit api service

--- data/src/main/java/com/revelet/data/remote/service/ApiService.kt
+++ data/src/main/java/com/revelet/data/remote/service/ApiService.kt
@@ -2,19 +2,12 @@ package com.revelet.data.remote.service
 
 import com.revelet.data.remote.dto.QuestionDto
 import com.revelet.data.remote.dto.QuestionTagsDto
-import com.revelet.data.remote.dto.QuizDto
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 
 interface ApiService {
-
-    @GET("quiz/{id}")
-    suspend fun getQuizById(
-        @Query("id") id: String,
-    ): Response<QuizDto>
-
 
     @GET("tags")
     suspend fun getAllTags(): Response<QuestionTagsDto>
@@ -30,6 +23,8 @@ interface ApiService {
     suspend fun getRandomSetOfQuestion(
         @Query("limit") limit: Int,
         @Query("categories") categories: String,
+        @Query("tags") tags: String,
+        @Query("types") types: String,
     ): Response<QuestionDto>
 
 }
