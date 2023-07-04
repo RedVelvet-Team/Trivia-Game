fbbd74bc8d4d7bcfbbe224b8bdd7a32998248544
Bassam <bassamouf@std.mans.edu.eg>
2023-07-05 02:38:13 +0300

    add colors

--- app/src/main/java/com/redvelvet/trivia_game/ui/theme/Color.kt
+++ app/src/main/java/com/redvelvet/trivia_game/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackgroundLight = Color(0xFFF6F6F6)
+val BackgroundDark = Color(0xFF2E3041)
+
+val ContainerLight = Color(0xF0F1F1F1)
+val ContainerDark = Color(0xFF37394B)
+
+val White = Color(0xF0F6F6F6)
+val Black = Color(0xFF000000)
+val DarkGray = Color(0xF07A7A7A)
+val LightGray = Color(0x99F6F6F6)
+
+val PrimaryColor = Color(0xF09850BD)
+val Red = Color(0xF0C83132)
+val Green = Color(0xF032925C)
