d68ee7454f2f747dfa567711603af0dca74434ff
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-07-05 22:32:25 +0300

    add start game usecase

--- domain/src/main/java/com/redvelvet/domain/usecases/StartGameUsecase.kt
+++ domain/src/main/java/com/redvelvet/domain/usecases/StartGameUsecase.kt
@@ -1,7 +1,26 @@
 package com.redvelvet.domain.usecases
 
+import com.redvelvet.domain.repository.ITriviaRepository
+import com.redvelvet.domain.utils.GameMode
 import javax.inject.Inject
 
 
 class StartGameUseCase @Inject constructor(
-) {}
+    private val gameSession: GameSession,
+    private val triviaRepository: ITriviaRepository,
+    private val casualModeUseCase: StartCasualGameUseCase,
+    private val timedModeUseCase: StartTimedGameUseCase,
+    private val survivalModeUseCase: StartSurvivalGameUseCase
+) {
+    suspend fun invoke(gameMode: GameMode) {
+        gameSession.isGameOver = false
+        gameSession.score = 0
+        triviaRepository.saveScore(gameSession.score)
+
+        when (gameMode) {
+            GameMode.CASUAL -> casualModeUseCase.invoke()
+            GameMode.TIMED -> timedModeUseCase.invoke()
+            GameMode.SURVIVAL -> survivalModeUseCase.invoke()
+        }
+    }
+}
---
+++ domain/src/main/java/com/redvelvet/domain/utils/GameMode.kt
@@ -0,0 +1,5 @@
+package com.redvelvet.domain.utils
+
+enum class GameMode {
+    CASUAL, TIMED, SURVIVAL
+}
