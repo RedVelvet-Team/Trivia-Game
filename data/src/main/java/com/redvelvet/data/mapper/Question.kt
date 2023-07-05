2a408ec42a55abc1e7b38630b9a8d2d37471ba25
Abanoub <abanoubnagy24@gmail.com>
2023-07-05 21:53:32 +0300

    rename question entity

--- data/src/main/java/com/redvelvet/data/mapper/Question.kt
+++ data/src/main/java/com/redvelvet/data/mapper/Question.kt
@@ -3,7 +3,7 @@ package com.redvelvet.data.mapper
 import com.redvelvet.data.remote.dto.QuestionDto
 import com.redvelvet.domain.entity.Question
 
-fun QuestionDto.toQuestionEntity() = Question(
+fun QuestionDto.toQuestion() = Question(
     category = category ?: "",
     correctAnswer = correctAnswer ?: "",
     difficulty = difficulty ?: "",
