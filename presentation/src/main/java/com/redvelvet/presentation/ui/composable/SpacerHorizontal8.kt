457e8413195d36a14fb037656ce2890cd822c2a9
Haidy <haidyabugom3a@gmail.com>
2023-07-07 00:29:59 +0300

    add spacers 8, 16

---
+++ presentation/src/main/java/com/redvelvet/presentation/ui/composable/SpacerHorizontal8.kt
@@ -0,0 +1,12 @@
+package com.redvelvet.presentation.ui.composable
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerHorizontal8(){
+    Spacer(modifier = Modifier.width(8.dp))
+}
