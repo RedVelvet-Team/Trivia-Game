d8c8f9113834322ebe4bc6489935c164eaf7af8f
Sadeq Al-Mhana <64174395+devsadeq@users.noreply.github.com>
2023-07-06 18:35:15 +0300

    create level chips composable

---
+++ presentation/src/main/java/com/redvelvet/presentation/ui/composable/CategoryLevelChips.kt
@@ -0,0 +1,32 @@
+package com.redvelvet.presentation.ui.composable
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.redvelvet.presentation.R
+
+@Composable
+fun CategoryLevelChips() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        val chips = listOf(
+            stringResource(R.string.easy),
+            stringResource(R.string.medium),
+            stringResource(R.string.difficult),
+        )
+        chips.forEachIndexed { index, label ->
+            Chip(
+                label = label,
+                onClick = {},
+                enabled = index == 0
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+    }
+}
