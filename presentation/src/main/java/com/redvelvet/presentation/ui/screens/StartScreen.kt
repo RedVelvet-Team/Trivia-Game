f207704f6d65ec74b93c818cd6d51057dc1e6fc1
Najeia Tammous <ntammous11@gmail.com>
2023-07-06 21:47:35 +0300

    create starting screen

---
+++ presentation/src/main/java/com/redvelvet/presentation/ui/screens/StartScreen.kt
@@ -0,0 +1,15 @@
+package com.redvelvet.presentation.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StartScreen(){
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun Preview(){
+    StartScreen()
+}
