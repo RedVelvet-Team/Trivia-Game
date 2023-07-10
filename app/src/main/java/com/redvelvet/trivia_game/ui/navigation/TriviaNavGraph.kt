77fa8440ef7bb10805ca166aca4bae6bf5411973
abaferas <wasfyelgamed@gmail.com>
2023-07-10 04:26:28 +0300

    [finish] test app running

--- app/src/main/java/com/redvelvet/trivia_game/ui/navigation/TriviaNavGraph.kt
+++ app/src/main/java/com/redvelvet/trivia_game/ui/navigation/TriviaNavGraph.kt
@@ -3,17 +3,15 @@ package com.redvelvet.trivia_game.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.redvelvet.trivia_game.ui.screen.category.SCREEN_KEY_CATEGORY_SCREEN
 import com.redvelvet.trivia_game.ui.screen.category.screenCategoryRoute
-import com.redvelvet.trivia_game.ui.screen.question.SCREEN_KEY_QUESTION_SCREEN
 import com.redvelvet.trivia_game.ui.screen.question.screenQuestionRoute
 
 
 @Composable
 fun TriviaNavGraph(navController: NavHostController){
-    NavHost(navController = navController, startDestination = SCREEN_KEY_QUESTION_SCREEN){
+    NavHost(navController = navController, startDestination = SCREEN_KEY_CATEGORY_SCREEN){
         screenCategoryRoute(navController)
         screenQuestionRoute(navController)
-
-
     }
 }
