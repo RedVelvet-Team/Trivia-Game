c2366841633718462f457bf9d9c39992cadcc961
abaferas <wasfyelgamed@gmail.com>
2023-07-11 23:59:43 +0300

    implement trivia game custom exceptions

--- app/src/main/java/com/redvelvet/trivia_game/domain/utils/TriviaException.kt
+++ app/src/main/java/com/redvelvet/trivia_game/domain/utils/TriviaException.kt
@@ -1,5 +1,8 @@
 package com.redvelvet.trivia_game.domain.utils
 
-sealed class TriviaException: Throwable() {
-    //TODO
+sealed class TriviaException(message: String) : Exception(message) {
+    class NoInternetConnection: TriviaException("No Internet Connection")
+    class ConnectionTimeOut: TriviaException("Connection TimeOut")
+    class NoContentFound: TriviaException("No Content Found")
+    class UnAuthorizedRequest: TriviaException("Un Authorized Request")
 }
