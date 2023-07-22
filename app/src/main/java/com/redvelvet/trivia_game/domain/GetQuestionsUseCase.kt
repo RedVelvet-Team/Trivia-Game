7b6352f29b0f60d5c501ad4529522dcd77e183df
abaferas <wasfyelgamed@gmail.com>
2023-07-23 02:30:34 +0300

    handle error in use case

--- app/src/main/java/com/redvelvet/trivia_game/domain/GetQuestionsUseCase.kt
+++ app/src/main/java/com/redvelvet/trivia_game/domain/GetQuestionsUseCase.kt
@@ -2,17 +2,22 @@ package com.redvelvet.trivia_game.domain
 
 import com.redvelvet.trivia_game.domain.entity.Question
 import com.redvelvet.trivia_game.domain.repository.ITriviaRepository
+import com.redvelvet.trivia_game.domain.utils.TriviaException
 import javax.inject.Inject
 
 class GetQuestionsUseCase @Inject constructor (
     private val iTriviaRepository: ITriviaRepository
 ) {
     suspend operator fun invoke(categories: String,difficulties : String): List<Question> {
-        return iTriviaRepository.getRandomSetOfQuestion(
-            limit = 10,
-            categories = categories,
-            difficulties = difficulties,
-            types = "text_choice"
-        )
+        return try {
+            iTriviaRepository.getRandomSetOfQuestion(
+                limit = 10,
+                categories = categories,
+                difficulties = difficulties,
+                types = "text_choice"
+            )
+        }catch (e: TriviaException){
+            throw e
+        }
     }
 }
